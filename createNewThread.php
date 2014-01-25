d68cac0695fc4d5fe04fb60822339ec2518d3e38
Zachary Friedlander <zachary.d.friedlander@gmail.com>
2014-01-25 04:26:04 -0800

    CreateNewThread updated - but how to get my own username?

--- createNewThread.php
+++ createNewThread.php
@@ -1,6 +1,10 @@
 <?php
 
-echo $_POST['name'];
+$newfile = fopen("threads/"+$_POST['name']+".cr", 'w');
+
+fwrite($newfile, /*username*/+" "+date("d/m/Y")+ $_POST['content');
+
+fclose($newfile);
 
 
 
