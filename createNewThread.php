10e5ee4330e4a04d3c79d35045750a610656f6b9
Zachary Friedlander <zachary.d.friedlander@gmail.com>
2014-01-25 07:16:41 -0800

    Jonathan, if you're right, I swear to god...

--- createNewThread.php
+++ createNewThread.php
@@ -4,6 +4,7 @@ require_once("conf.inc.php");
 $db_handle = mysqli_connect($CFG->hostname, $CFG->username, $CFG->password, $CFG->dbName);
 
 $newfile = fopen("/srv/http/threads/".$_POST['name'].".cr", 'w+') or die("Failure!");
+fwrite($newfile, "Jonathan if you're right, I swear to God...");
 $result = mysqli_query("SELECT username FROM users WHERE id='".$_SESSION['cur_id']."';");
 
 if ($worked = mysqli_fetch_assoc($result)) {
@@ -12,7 +13,7 @@ if ($worked = mysqli_fetch_assoc($result)) {
 	//now that the file has been created, we can write to it
 	$newfile = fopen("/srv/http/threads/".$_POST['name'].".cr", 'w+') or die("Failure!");
 	$t = time();
-	fwrite($newfile, $user." ".date("d/m/Y", $t).$_POST['content']);
+	fwrite($newfile, $user." ".date("d/m/Y", $Y).$_POST['content']);
 	fclose($newfile);
 }
 
