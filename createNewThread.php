458f84eb5cc3d05c16a811389671900efff2ba28
Zachary Friedlander <zachary.d.friedlander@gmail.com>
2014-01-25 05:51:09 -0800

    grr

--- createNewThread.php
+++ createNewThread.php
@@ -3,7 +3,7 @@
 require_once("conf.inc.php");
 $db_handle = mysqli_connect($CFG->hostname, $CFG->username, $CFG->password, $CFG->dbName);
 
-$newfile = fopen("threads/"+$_POST['name']+".cr", 'w+');
+$newfile = fopen("threads/"+$_POST['name']+".cr", 'w');
 $result = mysqli_query("SELECT username FROM users WHERE id='".$_SESSION['cur_id']."';");
 
 if ($worked = mysqli_fetch_assoc($result)) {
