561574fb42076f26e39a2ce5ed6954153a096d23
Zachary Friedlander <zamru713@gmail.com>
2014-01-25 12:06:13 -0500

    Pretty thread views

--- view_thread.php
+++ view_thread.php
@@ -1,73 +1,83 @@
-
-
+<?php
+require_once('conf.inc.php');
+session_start();
+?>
 
 <html>
+<head>
+  <title><?php echo $_GET['filename']." - OurCorner"; ?></title>
+  <link rel="stylesheet" type="text/css" href="styles/ourcorner.css" />
+</head>
 <body>
 
 
 
-<div>
-
-<h1>OurCorner</h1>
-
-<h5>Muddysoul</h5>
+<div style="height:120px;">
+<div class="logo">
+<img src="img/logo.png" height="100px" />
+</div>
+<div class="usercontrol">
+Hello, <? php echo $_SESSION['cur_user']; ?>
+<br />
+<br />
 
 <form action="logout.php" method="post">
 <input type="submit" value="Logout">
 </form>
-
+</div>
 </div>
 
-
-
+<div>
+<div class="sidebar">
 <div>
 <h3>Actions</h3>
-<a href="newThread.html">New Thread</a>
++ <a href="newThread.html">New Thread</a>
 <br />
-<a href="URL">My Account</a>
++ <a href="URL">My Account</a>
 </div>
 
 
 
 <div>
 <h3>My Threads</h3>
-<a href="URL">I heart sewing</a>
++ <a href="URL">I heart sewing</a>
 <br />
-<a href="URL">SUPERNATURAL!!</a>
++ <a href="URL">SUPERNATURAL!!</a>
+</div>
 </div>
 
-
-
+<div class="main">
 <?php
 $corner=fopen("threads/".$_GET['filename'].".cr", "r");
-echo $_GET['filename'];
-
+echo "<h2>".$_GET['filename']."</h2>";
+echo "<div style=\"align:center;\">";
 while(!feof($corner))
 {
 $post = fgets($corner);
 $space = strpos($post, " ");
 $name = substr($post, 0, $space);
-echo $name;
 
 $post = substr($post, $space+1);
 $space = strpos($post, " ");
 $date = substr($post, 0, $space);
-echo $date;
 
 $post = substr($post, $space+1);
 $space = strpos($post, " ");
 $time = substr($post, 0, $space);
-echo $time;
 
 $rest = substr($post, $space+1);
-echo $rest;
-}
 
+echo "<div class=\"response\">";
+echo "<b>".$name."</b> on ".$date." at ".$time."<br /><br />";
+echo $rest;
+echo "</div>";
+}
+echo "</div>";
 fclose($file);
 ?>
+</div>
 
-
-
+</div>
 
 
 
