bd26f5b87b030232802ea941c16358b3241f9007
Zachary Friedlander <zachary.d.friedlander@gmail.com>
2014-01-25 08:08:03 -0800

    stop - error time.

--- createNewThread.php
+++ createNewThread.php
@@ -3,10 +3,12 @@
 require_once('conf.inc.php');
 session_start();
 $db_handle = mysqli_connect($CFG->hostname, $CFG->username, $CFG->password, $CFG->dbName);
-
+if msqyli_connect_errno($db_handle) {
+	echo "Uh oh...";
+}
 $newfile = fopen("/srv/http/threads/".$_POST['name'].".cr", 'w+') or die("Failure!");
 echo $_SESSION['cur_id'];
-$result = mysqli_query("SELECT username FROM users WHERE id='0';");
+$result = mysqli_query("SELECT username FROM users WHERE id='".$_SESSION['cur_id']."';");
 
 if ($worked = mysqli_fetch_assoc($result)) {
 	echo "WOOHOOO";
