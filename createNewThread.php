0de17c36b98a7323223815730215be523d412da6
Zachary Friedlander <zamru713@gmail.com>
2014-01-25 07:08:50 -0500

    readded createThread.php - because somehow it disapparated.

---
+++ createNewThread.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once("conf.inc.php");
+$db_handle = mysqli_connect($CFG->hostname, $CFG->username, $CFG->password, $CFG->dbName);
+
+$newfile = fopen("/srv/http/threads/".$_POST['name'].".cr", 'w') or die("Failure!");
+$result = mysqli_query("SELECT username FROM users WHERE id='".$_SESSION['cur_id']."';");
+
+if ($worked = mysqli_fetch_assoc($result)) {
+	$user = $worked['username'];
+	fwrite($newfile, $user." ".date("d/m/Y", time()).$_POST['content']);
+	fclose($newfile);
+}
+
+header('Location: home_page.php');
+
+
+
+?>
