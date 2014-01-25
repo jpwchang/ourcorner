3a826036b7418e79a11816117dc443e285ea58d5
Zachary Friedlander <zachary.d.friedlander@gmail.com>
2014-01-25 01:53:50 -0800

    Now with Homepage

---
+++ home_page.php
@@ -0,0 +1,64 @@
+
+
+
+<html>
+<body>
+
+
+
+<div>
+
+<h1>OurCorner</h1>
+
+<form action="logout.php" method="post">
+<input type="submit" value="Logout">
+</form>
+
+</div>
+
+
+
+<div>
+<h3>Actions</h3>
+<a href="newThread.html">New Thread</a>
+<a href="URL">My Account</a>
+</div>
+
+
+
+<div>
+<h3>My Threads</h3>
+<a href="URL">I heart sewing</a>
+<a href="URL">SUPERNATURAL!!</a>
+</div>
+
+
+
+<!-- switch to tabs later? -->
+<!-- Drop-down menu to allow user to choose which threads to view -->
+<div>
+<form action="">
+<select name="threads">
+<option value="recent">Recent Threads</option>
+<option value="my">My Threads</option>
+<option value="subscribed">Subscribed Threads</option>
+</select>
+</form>
+</div>
+
+
+
+<div>
+<a href="URL">Fuck Cats</a>
+<p>Members: Zach, Jonathan</p>
+</div>
+
+<div>
+<a href="URL">Cats are dabes</a>
+<p>Members: Adam, Paige</p>
+</div>
+
+
+
+</body>
+</html>
