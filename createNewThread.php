7f82d5465b3d131252440f33909bc2ec14542f6d
Zachary Friedlander <zachary.d.friedlander@gmail.com>
2014-01-25 07:41:46 -0800

    testing time

--- createNewThread.php
+++ createNewThread.php
@@ -6,6 +6,7 @@ $db_handle = mysqli_connect($CFG->hostname, $CFG->username, $CFG->password, $CFG
 
 $newfile = fopen("/srv/http/threads/".$_POST['name'].".cr", 'w+') or die("Failure!");
 fwrite($newfile, "Jonathan if you're right, I swear to God...");
+echo $_SESSION['cur_id'];
 $result = mysqli_query("SELECT username FROM users WHERE id='".$_SESSION['cur_id']."';");
 
 if ($worked = mysqli_fetch_assoc($result)) {
